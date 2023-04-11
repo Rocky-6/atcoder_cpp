faf8350d071f155d84ce9d9ae2583df12271f5ec
Rocky-6 <imu387058@gmail.com>
2023-04-11 19:58:38 +0900

    c

--- abc285/c.cpp
+++ abc285/c.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
 
+    long long ans = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        long long tmp = s[i] - 'A' + 1;
+        ans = ans * 26 + tmp;
+    }
+    cout << ans << endl;
     return 0;
 }
