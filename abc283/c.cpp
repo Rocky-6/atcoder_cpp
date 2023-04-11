fd7b4735177cc2882b4f76867ff784289c1db1af
Rocky-6 <imu387058@gmail.com>
2023-04-11 20:59:59 +0900

    c

--- abc283/c.cpp
+++ abc283/c.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '0') {
+            if (s[i+1] == '0') {
+                ans++; i++;
+            } else {
+                ans++;
+            }
+        } else {
+            ans++;
+        }
+    }
 
+    cout << ans << endl;
+    
     return 0;
 }
