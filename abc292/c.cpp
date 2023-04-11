c656a9c0ea7d6526796654026bef5a864544c00a
Rocky-6 <imu387058@gmail.com>
2023-04-11 18:51:43 +0900

    c

--- abc292/c.cpp
+++ abc292/c.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main() {
+	int n;
+	cin >> n;
 
-    return 0;
+	long long ans = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		int root_ab = int(sqrt(i));
+		int root_cd = int(sqrt(n - i));
+		int num_ab = 0, num_cd = 0;
+		int ab = i, cd = n - i;
+
+		for (int j = 1; j <= root_ab; j++)
+		{
+			if (ab % j == 0)
+			{
+				if (ab / j == j)
+				{
+					num_ab++;
+				}
+				else
+				{
+					num_ab += 2;
+				}
+			}
+		}
+		for (int j = 1; j <= root_cd; j++)
+		{
+			if (cd % j == 0)
+			{
+				if (cd / j == j)
+				{
+					num_cd++;
+				}
+				else
+				{
+					num_cd += 2;
+				}
+			}
+		}
+
+		ans += num_ab * num_cd;
+	}
+
+	cout << ans << endl;
+	return 0;
 }
--- abc293/c.cpp
+++ abc293/c.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 int main() {
+	int h, w;
+	cin >> h >> w;
+	int a[11][11];
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	
+	int ans = 0;
+	for (int bit = 0; bit < (1<<(h+w-2)); bit++)
+	{
+		int flag = 0;
+		for (int i = 0; i < h+w-2; i++)
+		{
+			if (bit & (1<<i))
+			{
+				flag++;
+			}
+		}
+		if (flag != h-1)
+		{
+			continue;
+		}
 
-    return 0;
+		set<int> s;
+		s.insert(a[0][0]);
+		int x = 0, y = 0;
+		for (int i = 0; i < h+w-2; i++)
+		{
+			if (bit & (1<<i))
+			{
+				y++;
+			}
+			else
+			{
+				x++;
+			}
+			s.insert(a[y][x]);
+		}
+		if (s.size() == h+w-2+1)
+		{
+			ans++;
+		}
+	}
+
+	cout << ans << endl;
+	return 0;
 }
