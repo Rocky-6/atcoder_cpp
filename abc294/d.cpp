82738c4b27d823f9c490f9fb36078529ffcff513
Rocky-6 <imu387058@gmail.com>
2023-04-14 18:48:16 +0900

    d

--- abc294/d.cpp
+++ abc294/d.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    int n, q;
+    cin >> n >> q;
+    set<int> yet, called;
+    for (int i = 0; i < n; i++) {
+        yet.insert(i + 1);
+    }
 
+    for (int i = 0; i < q; i++) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int x = *yet.begin();
+            yet.erase(x);
+            called.insert(x);
+        }
+        if (type == 2) {
+            int x;
+            cin >> x;
+            called.erase(x);
+        }
+        if (type == 3) {
+            int x = *called.begin();
+            cout << x << endl;
+        }
+    }
     return 0;
 }
