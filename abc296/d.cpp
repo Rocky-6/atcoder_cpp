33ad1183b84998b58e278d83621203c16f69feff
Rocky-6 <imu387058@gmail.com>
2023-04-14 16:23:12 +0900

    d

--- abc296/d.cpp
+++ abc296/d.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
-int main() {
+long long INF = 1e18;
 
+int main() {
+    long long n, m;
+    cin >> n >> m;
+    long long ans = INF;
+    for (long long a = 1; a <= m / a + 1; a++) {
+        long long b = (m + a - 1) / a;
+        if (a <= n && b <= n) ans = min(ans, a * b);
+    }
+    if (ans == INF) ans = -1;
+
+    cout << ans << endl;
     return 0;
 }
