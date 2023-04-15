f31ba29a02dba9b8c12e9f3d6ad4cb107bf7b914
Rocky-6 <imu387058@gmail.com>
2023-04-15 18:03:46 +0900

    d

--- abc292/d.cpp
+++ abc292/d.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> G(n);
+    vector<int> deg(n, 0);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        G[u].push_back(v);
+        G[v].push_back(u);
+        deg[u]++;
+    }
 
+    vector<bool> seen(n, false);
+    string ans = "Yes";
+    for (int i = 0; i < n; i++) {
+        if (seen[i]) continue;
+        seen[i] = true;
+        queue<int> que;
+        que.push(i);
+        int ver = 1;
+        int degs = 0;
+        while (!que.empty()) {
+            int q = que.front();
+            que.pop();
+            degs += deg[q];
+            for (auto v : G[q]) {
+                if (seen[v]) continue;
+                seen[v] = true;
+                ver++;
+                que.push(v);
+            }
+        }
+        if (degs != ver) ans = "No";
+    }
+
+    cout << ans << endl;
     return 0;
 }
