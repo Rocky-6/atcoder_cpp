5d1f04fbce3eda0c9ab5af6fcee8d853ac72d717
Rocky-6 <imu387058@gmail.com>
2023-04-16 11:00:20 +0900

    d

--- abc290/d.cpp
+++ abc290/d.cpp
@@ -1,8 +1,17 @@
-#include <bits/stdc++>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int n, d, k;
+        cin >> n >> d >> k;
+        k--;
+        cout << (long long)k * d % n + k / (n / gcd(n, d)) << endl;
+    }
 
     return 0;
 }
