bf5453b0b93272711ddf44c35a9df32bb3b1db04
Rocky-6 <imu387058@gmail.com>
2023-04-19 09:32:33 +0900

    d

--- abc287/d.cpp
+++ abc287/d.cpp
@@ -2,7 +2,50 @@
 
 using namespace std;
 
+bool is_match_char(char cs, char ct) {
+    return cs == ct || cs == '?' || ct == '?';
+}
+
 int main() {
+    string s;
+    cin >> s;
+    string t;
+    cin >> t;
+
+    vector<bool> start(t.size(), false), end(t.size(), false);
+    for (int i = 0; i < t.size(); i++) {
+        if (!is_match_char(s[i], t[i])) break;
+        start[i] = true;
+    }
+
+    reverse(s.begin(), s.end());
+    reverse(t.begin(), t.end());
+
+    for (int i = 0; i < t.size(); i++) {
+        if (!is_match_char(s[i], t[i])) break;
+        end[i] = true;
+    }
+
+    for (int i = 0; i <= t.size(); i++) {
+        if (i == 0) {
+            if (end[t.size() - 1])
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
+        }
+        if (i > 0 && i < t.size()) {
+            if (start[i - 1] && end[t.size() - i - 1])
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
+        }
+        if (i == t.size()) {
+            if (start[i - 1])
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
+        }
+    }
 
     return 0;
 }
