31999a59d47cf04bdf38986f4f44b153b236c251
Rocky-6 <imu387058@gmail.com>
2023-04-21 17:55:28 +0900

    c

--- abc276/c.cpp
+++ abc276/c.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> p;
+
+    for (int i = 0; i < n; i++) {
+        int pi;
+        cin >> pi;
+        p.push_back(pi);
+    }
+
+    prev_permutation(p.begin(), p.end());
+    for (auto ans : p) cout << ans << ' ';
+    cout << endl;
 
     return 0;
 }
--- abc277/c.cpp
+++ abc277/c.cpp
@@ -3,6 +3,33 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    map<int, vector<int>> G;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
 
+    map<int, bool> seen;
+    queue<int> que;
+    seen[1] = true;
+    que.push(1);
+    int m = 1;
+    while (!que.empty()) {
+        int v = que.front();  // キューから先頭頂点を取り出す
+        que.pop();
+        // v から辿れる頂点をすべて調べる
+        for (int nv : G[v]) {
+            if (seen[nv]) continue;
+            seen[nv] = true;
+            que.push(nv);
+            m = max(m, nv);
+        }
+    }
+
+    cout << m << endl;
     return 0;
 }
