29763656485636a3573e549ab21de5294a3f8b05
Rocky-6 <imu387058@gmail.com>
2023-04-21 16:33:06 +0900

    c

--- abc278/c.cpp
+++ abc278/c.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
+    int n, q;
+    cin >> n >> q;
+    map<int, set<int>> G;
+    for (int i = 0; i < q; i++) {
+        int t, a, b;
+        cin >> t >> a >> b;
+        if (t == 1) G[a].insert(b);
+        if (t == 2) G[a].erase(b);
+        if (t == 3) {
+            if (G[a].find(b) != G[a].end() && G[b].find(a) != G[b].end())
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
+        }
+    }
 
     return 0;
 }
