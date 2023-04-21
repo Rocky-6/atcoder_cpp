503fee5490ad4b87c77b67fa35a18ae12c3fd8fc
Rocky-6 <imu387058@gmail.com>
2023-04-21 17:55:21 +0900

    b

--- abc276/b.cpp
+++ abc276/b.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> G(n);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << G[i].size() << ' ';
+        sort(G[i].begin(), G[i].end());
+        for (auto nv : G[i]) cout << nv + 1 << ' ';
+        cout << endl;
+    }
 
     return 0;
 }
--- abc277/b.cpp
+++ abc277/b.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    set<string> st;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        st.insert(s);
+        if (s[0] != 'H' && s[0] != 'D' && s[0] != 'C' && s[0] != 'S') {
+            cout << "No" << endl;
+            return 0;
+        }
+        if (s[1] != 'A' && !(s[1] >= '2' && s[1] <= '9') && s[1] != 'T' &&
+            s[1] != 'J' && s[1] != 'Q' && s[1] != 'K') {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    if (st.size() != n)
+        cout << "No" << endl;
+    else
+        cout << "Yes" << endl;
 
     return 0;
 }
