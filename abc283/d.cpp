fb205ac572b3f6861e0bf0de62f2b9dcd4079cf6
Rocky-6 <imu387058@gmail.com>
2023-04-26 18:20:34 +0900

    d

--- abc283/d.cpp
+++ abc283/d.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+
+    stack<string> st;
+    set<char> cs;
+    st.push("");
+    for (char c : s) {
+        if (c == '(') {
+            st.push("");
+        } else if (c == ')') {
+            for (char x : st.top()) {
+                cs.erase(x);
+            }
+            st.pop();
+        } else {
+            if (cs.count(c)) {
+                cout << "No" << endl;
+                return 0;
+            }
+            cs.insert(c);
+            st.top() += c;
+        }
+    }
+    cout << "Yes" << endl;
 
     return 0;
 }
--- abc284/d.cpp
+++ abc284/d.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        long long p = 0, q = 0;
+        for (long long i = 2; i < n; i++) {
+            if (n % i != 0) continue;
+            if ((n / i) % i == 0) {
+                p = i;
+                q = n / i / i;
+            } else {
+                q = i;
+                p = sqrt(n / i);
+            }
+            break;
+        }
+        cout << p << ' ' << q << endl;
+    }
 
     return 0;
 }
