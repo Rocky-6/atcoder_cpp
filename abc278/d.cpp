66b6613ef33f313ad67f1d3a5d4b712d2ea83004
Rocky-6 <imu387058@gmail.com>
2023-05-10 18:53:46 +0900

    d

--- abc278/d.cpp
+++ abc278/d.cpp
@@ -3,6 +3,44 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    vector<bool> clear(n);
+    int all;
+    for (auto& nx : a) cin >> nx;
+    int q;
+    cin >> q;
+    while (q--) {
+        int query;
+        cin >> query;
+        if (query == 1) {
+            int x;
+            cin >> x;
+            all = x;
+            clear.assign(n, true);
+        }
+        if (query == 2) {
+            int i, x;
+            cin >> i >> x;
+            i--;
+            if (clear[i]) {
+                a[i] = all + x;
+                clear[i] = false;
+            } else {
+                a[i] += x;
+            }
+        }
+        if (query == 3) {
+            int i;
+            cin >> i;
+            i--;
+            if (clear[i])
+                cout << all << endl;
+            else
+                cout << a[i] << endl;
+        }
+    }
 
     return 0;
 }
--- abc279/d.cpp
+++ abc279/d.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main() {
+    long long a, b;
+    cin >> a >> b;
+    auto f = [&](long long n) -> double {
+        return (double)a / sqrt(n + 1) + (double)b * n;
+    };
+    long long l = 0, r = a / b;
+    while (r - l > 2) {
+        long long m1 = (l * 2 + r) / 3;
+        long long m2 = (l + r * 2) / 3;
+        if (f(m1) > f(m2))
+            l = m1;
+        else
+            r = m2;
+    }
+
+    double ans = a;
+
+    for (long long i = l; i <= r; i++) {
+        ans = min(ans, f(i));
+    }
+
+    cout << fixed << setprecision(10) << ans << endl;
 
     return 0;
 }
