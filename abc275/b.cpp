923f4172cd65bc86d03e3a9e11602691df9b914a
Rocky-6 <imu387058@gmail.com>
2023-05-10 18:55:43 +0900

    b

--- abc275/b.cpp
+++ abc275/b.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
-
+    long long mod = 998244353;
+    long long a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    a %= mod;
+    b %= mod;
+    c %= mod;
+    d %= mod;
+    e %= mod;
+    f %= mod;
+    auto left = (((a * b) % mod) * c) % mod;
+    auto right = (((d * e) % mod) * f) % mod;
+    long long ans = (left - right) % mod;
+    if (ans < 0) ans += mod;
+    cout << ans << endl;
     return 0;
 }
