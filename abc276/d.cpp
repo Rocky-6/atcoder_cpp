c350c895a2ba2fc10260928b4a1b6339058b80b9
Rocky-6 <imu387058@gmail.com>
2023-05-11 11:14:12 +0900

    d

--- abc276/d.cpp
+++ abc276/d.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int g = 0;
+    for (auto& nx : a) {
+        cin >> nx;
+        g = gcd(g, nx);
+    }
+    int ans = 0;
+    for (auto& nx : a) {
+        nx /= g;
+        while (nx % 2 == 0) {
+            nx /= 2;
+            ans++;
+        }
+        while (nx % 3 == 0) {
+            nx /= 3;
+            ans++;
+        }
+        if (nx != 1) {
+            cout << -1 << endl;
+            return 0;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
