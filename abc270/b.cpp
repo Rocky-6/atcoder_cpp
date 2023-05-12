28d8bd8b546756ea4373f88aacbf0f81a1c8779e
Rocky-6 <imu387058@gmail.com>
2023-05-12 15:26:26 +0900

    b

--- abc270/b.cpp
+++ abc270/b.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (x < 0) {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+    int ans;
+    if (y < 0 && 0 < x)
+        ans = x;
+    else if (0 < y && y < x) {
+        if (z < y)
+            ans = abs(z) + abs(x - z);
+        else
+            ans = -1;
+    }
+    cout << ans << endl;
     return 0;
 }
