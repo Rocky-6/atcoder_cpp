2d5c8aecbccd4b618986ac1f9a68dfbde627c8a6
Rocky-6 <imu387058@gmail.com>
2023-05-15 14:16:38 +0900

    a

--- abc301/a.cpp
+++ abc301/a.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int t = 0, a = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'T')
+            t++;
+        else
+            a++;
+        if (s.size() % 2 == 0) {
+            if (t == s.size() / 2 || a == s.size() / 2) break;
+        }
+    }
+    if (t > a)
+        cout << 'T' << endl;
+    else
+        cout << 'A' << endl;
     return 0;
 }
