23de7a88d039356a1cc110e8f92f562c3be35385
Rocky-6 <imu387058@gmail.com>
2023-05-15 14:16:46 +0900

    b

--- abc301/b.cpp
+++ abc301/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ax : a) cin >> ax;
+    for (int i = 0; i < n - 1; i++) {
+        cout << a[i] << ' ';
+        if (abs(a[i + 1] - a[i]) > 1) {
+            if (a[i] > a[i + 1]) {
+                for (int j = a[i] - 1; j > a[i + 1]; j--) cout << j << ' ';
+            } else {
+                for (int j = a[i] + 1; j < a[i + 1]; j++) cout << j << ' ';
+            }
+        }
+    }
+    cout << a[n - 1] << endl;
     return 0;
 }
