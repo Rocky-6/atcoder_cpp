6e6a2180e2582e0edf3d5545112d49c470ac7ab2
Rocky-6 <imu387058@gmail.com>
2023-05-18 15:56:07 +0900

    d

--- abc275/d.cpp
+++ abc275/d.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
+    ll n;
+    cin >> n;
+    map<ll, ll> memo;
 
+    auto f = [&](auto f, ll n) -> ll {
+        if (n == 0) return 1;
+        if (memo.count(n) != 0) return memo[n];
+        memo[n] = f(f, n / 2) + f(f, n / 3);
+        return memo[n];
+    };
+
+    cout << f(f, n) << endl;
     return 0;
 }
