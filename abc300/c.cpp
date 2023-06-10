7d42ee875414a4bc90149922fc3a1b098b146296
Rocky-6 <imu387058@gmail.com>
2023-06-10 16:19:21 +0900

    c

--- abc300/c.cpp
+++ abc300/c.cpp
@@ -5,40 +5,42 @@ using namespace std;
 int main() {
     int h, w;
     cin >> h >> w;
-    vector<vector<char>> c(h, vector<char>(w));
-    vector<vector<bool>> seen(h, vector<bool>(w));
+    vector c(h, vector<char>(w));
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             cin >> c[i][j];
         }
     }
-    map<int, int> ans;
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (c[i][j] == '.') continue;
-            if (seen[i][j]) continue;
-            seen[i][j] = true;
-            char x = c[i][j];
-            int k = 1;
-            while (1) {
-                if (i + k == h || j + k == w) break;
-                x = c[i + k][j + k];
-                if (x == '.') break;
-                seen[i + k][j + k] = true;
-                k++;
-            }
-            ans[k / 2]++;
-            for (int l = 0; l < k / 2; l++) {
-                seen[i + (k / 2) * 2 - l][j + l] = true;
-                seen[i + l][j + (k / 2) * 2 - l] = true;
+    vector<int> ans(min(h, w) + 1);
+    for (int i = 1; i < h - 1; i++) {
+        for (int j = 1; j < w - 1; j++) {
+            if (c[i][j] == '#') {
+                int k = 1;
+                bool flag = true;
+                while (flag) {
+                    for (auto x : {-1, 1}) {
+                        for (auto y : {-1, 1}) {
+                            if (i + x * k >= h || i + x * k < 0) {
+                                flag = false;
+                                break;
+                            }
+                            if (j + y * k >= w || j + y * k < 0) {
+                                flag = false;
+                                break;
+                            }
+                            if (c[i + x * k][j + y * k] != '#') flag = false;
+                        }
+                    }
+                    if (!flag) ans[k - 1]++;
+                    k++;
+                }
             }
         }
     }
-    int a = min(h, w);
-    for (int i = 1; i <= min(h, w); i++) {
-        cout << ans[i] << ' ';
-    }
+
+    for (int i = 0; i < min(h, w); i++) cout << ans[i + 1] << ' ';
 
     cout << endl;
+
     return 0;
 }
