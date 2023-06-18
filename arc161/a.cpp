6c807f8e94c3613170d7992118378d1fe18f5694
Rocky-6 <imu387058@gmail.com>
2023-06-18 19:41:32 +0900

    a

--- arc161/a.cpp
+++ arc161/a.cpp
@@ -12,11 +12,12 @@ int main() {
     sort(a.begin(), a.end());
     for (int i = 0; i <= n / 2; i++) x[2 * i] = a[i];
     for (int i = 0; i <= n / 2 - 1; i++) x[2 * i + 1] = a[n / 2 + 1 + i];
-    for (int i = 1; i < n; i = i + 2)
+    for (int i = 1; i < n; i = i + 2) {
         if (x[i] <= x[i - 1] || x[i] <= x[i + 1]) {
             cout << "No" << endl;
             return 0;
         }
+    }
     cout << "Yes" << endl;
     return 0;
 }
