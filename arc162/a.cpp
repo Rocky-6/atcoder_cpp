b92cd5ed5ac0df854ddf380a0d10211355314c2d
Rocky-6 <imu387058@gmail.com>
2023-06-19 12:10:45 +0900

    a

--- arc162/a.cpp
+++ arc162/a.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<pair<int, int>> pa(n);
+        for (int i = 0; i < n; i++) {
+            int p;
+            cin >> p;
+            p--;
+            pa[i] = pair<int, int>(i, p);
+        }
+
+        sort(pa.begin(), pa.end(), [](const auto& lhs, const auto& rhs) {
+            return lhs.second < rhs.second;
+        });
+
+        int pivot = pa[0].first;
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            if (pivot > pa[i].first) continue;
+            pivot = pa[i].first;
+            ans++;
+        }
+        cout << ans << endl;
+    }
 
     return 0;
 }
