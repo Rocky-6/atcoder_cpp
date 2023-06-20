dd10c28a3b54358e403c6ce381b5257d4e4e2b38
Rocky-6 <imu387058@gmail.com>
2023-06-20 20:08:10 +0900

    c

--- edpc/c.cpp
+++ edpc/c.cpp
@@ -1,5 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
-int main() { return 0; }
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n + 1), b(n + 1), c(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i] >> b[i] >> c[i];
+    }
+
+    ll dp[100005][3];
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < 3; j++) {
+            dp[i][j] = -1e18;
+        }
+    }
+    dp[1][0] = a[1];
+    dp[1][1] = b[1];
+    dp[1][2] = c[1];
+
+    for (int i = 1; i < n; i++) {
+        dp[i + 1][0] = max(dp[i][1], dp[i][2]) + a[i + 1];
+        dp[i + 1][1] = max(dp[i][0], dp[i][2]) + b[i + 1];
+        dp[i + 1][2] = max(dp[i][0], dp[i][1]) + c[i + 1];
+    }
+
+    cout << max(max(dp[n][0], dp[n][1]), dp[n][2]) << endl;
+
+    return 0;
+}
