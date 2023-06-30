f04face22af2dec0b2744b87d7951196b4009048
Rocky-6 <imu387058@gmail.com>
2023-06-30 18:35:29 +0900

    abc307

---
+++ abc307/c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int ha, wa;
+    cin >> ha >> wa;
+    vector<string> a(ha);
+    for (int i = 0; i < ha; i++) {
+        cin >> a[i];
+    }
+    int hb, wb;
+    cin >> hb >> wb;
+    vector<string> b(hb);
+    for (int i = 0; i < hb; i++) {
+        cin >> b[i];
+    }
+    int hx, wx;
+    cin >> hx >> wx;
+    vector<string> x(hx);
+    for (int i = 0; i < hx; i++) {
+        cin >> x[i];
+    }
+
+        return 0;
+}
