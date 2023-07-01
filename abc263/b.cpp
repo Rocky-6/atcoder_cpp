3518a95661ca6206b07c94d772b8341b2ff5f50b
Rocky-6 <imu387058@gmail.com>
2023-07-01 18:29:26 +0900

    b

--- abc263/b.cpp
+++ abc263/b.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cin >> n;
+    vector<int> p(n + 1);
+    for (int i = 2; i <= n; i++) {
+        cin >> p[i];
+    }
+    int ans = 0;
+    int i = n;
+    while (i != 1) {
+        ans++;
+        i = p[i];
+    }
+    cout << ans << endl;
     return 0;
 }
