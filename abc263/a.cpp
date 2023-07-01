1f7ed0f972d7c04a2849e36374da626997c7b61a
Rocky-6 <imu387058@gmail.com>
2023-07-01 18:29:21 +0900

    a

--- abc263/a.cpp
+++ abc263/a.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 
 int main() {
+    int n[13] = {0};
+    for (int i = 0; i < 5; i++) {
+        int a;
+        cin >> a;
+        a--;
+        n[a]++;
+    }
+    int x[6] = {0};
+    for (int i = 0; i < 13; i++) {
+        x[n[i]]++;
+    }
 
+    for (int i = 1; i < 6; i++) {
+        if (i == 2 && x[i] != 1) {
+            cout << "No" << endl;
+            return 0;
+        }
+        if (i == 3 && x[i] != 1) {
+            cout << "No" << endl;
+            return 0;
+        }
+        if (i != 2 && i != 3 && x[i] != 0) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
     return 0;
 }
