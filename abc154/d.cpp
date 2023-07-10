98c4cc1d5d5ba45a6e542b691586890182e7b51e
Rocky-6 <imu387058@gmail.com>
2023-07-10 10:43:53 +0000

    d

--- abc154/d.cpp
+++ abc154/d.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> p(n);
+    vector<double> sum(n + 1);
+    sum[0] = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+        double exp = (double)p[i] * (p[i] + 1) / 2 / p[i];
+        sum[i + 1] = sum[i] + exp;
+    }
 
+    double M = 0;
+    for (int i = k; i <= n; i++) {
+        M = max(M, sum[i] - sum[i - k]);
+    }
+
+    cout << setprecision(10) << M << endl;
     return 0;
 }
