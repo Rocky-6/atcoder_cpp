e61174b4b48770f56348e8f2897f0f118bd2882c
Rocky-6 <imu387058@gmail.com>
2023-07-22 23:16:32 +0900

    c

--- abc274/c.cpp
+++ abc274/c.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    map<int, int> mp;
+    mp[1] = 0;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        mp[2 * i] = mp[a] + 1;
+        mp[2 * i + 1] = mp[a] + 1;
+    }
 
+    for (auto itr = mp.begin(); itr != mp.end(); itr++) {
+        cout << itr->second << endl;
+    }
     return 0;
 }
