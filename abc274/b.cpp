52c7052215971aa430896e224c525c454cb3660a
Rocky-6 <imu387058@gmail.com>
2023-07-22 23:16:24 +0900

    b

--- abc274/b.cpp
+++ abc274/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<int> row(w, 0);
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            char c;
+            cin >> c;
+            if (c == '#') row[j]++;
+        }
+    }
 
+    for (int i = 0; i < w; i++) {
+        cout << row[i] << ' ';
+    }
+    cout << endl;
     return 0;
 }
