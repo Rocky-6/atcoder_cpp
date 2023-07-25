c704e98599bab946dad4167260d6ea3079c0d6ea
Rocky-6 <imu387058@gmail.com>
2023-07-26 00:27:17 +0900

    c

--- abc202/c.cpp
+++ abc202/c.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
+    int n;
+    cin >> n;
+    map<ll, ll> mpa;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        mpa[a]++;
+    }
+    map<ll, ll> mpb;
+    for (int i = 1; i <= n; i++) {
+        int b;
+        cin >> b;
+        if (mpa.count(b) != 0) mpb[i] = mpa[b];
+    }
+    map<ll, ll> mpc;
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        int c;
+        cin >> c;
+        ans += mpb[c];
+    }
+    cout << ans << endl;
 
     return 0;
 }
