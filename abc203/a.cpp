85dbd8f749b1ce60cacbfe274a784d8d94c36159
Rocky-6 <imu387058@gmail.com>
2023-07-26 00:27:23 +0900

    a

--- abc203/a.cpp
+++ abc203/a.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-
+    int a, b, c;
+    cin >> a >> b >> c;
+    map<int, int> mp;
+    mp[a]++;
+    mp[b]++;
+    mp[c]++;
+    if (mp.size() == 3)
+        cout << 0 << endl;
+    else {
+        if (mp[a] != 2)
+            cout << a << endl;
+        else if (mp[b] != 2)
+            cout << b << endl;
+        else
+            cout << c << endl;
+    }
     return 0;
 }
