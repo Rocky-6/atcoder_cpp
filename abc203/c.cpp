10860a086964c7de638044b11d2076ddcf6ff7f9
Rocky-6 <imu387058@gmail.com>
2023-07-26 00:27:29 +0900

    c

--- abc203/c.cpp
+++ abc203/c.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
+    int n;
+    ll k;
+    cin >> n >> k;
+    map<ll, ll> ab;
+    for (int i = 0; i < n; i++) {
+        ll a, b;
+        cin >> a >> b;
+        ab[a] += b;
+    }
 
+    for (auto itr = ab.begin(); itr != ab.end(); itr++) {
+        if (itr->first <= k)
+            k += itr->second;
+        else {
+            break;
+        }
+    }
+    cout << k << endl;
     return 0;
 }
