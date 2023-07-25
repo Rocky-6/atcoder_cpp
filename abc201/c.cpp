1695306b725a2a0df17ef9f6eb6992a55a09aebe
Rocky-6 <imu387058@gmail.com>
2023-07-25 15:50:37 +0900

    c

--- abc201/c.cpp
+++ abc201/c.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+    set<int> aru, wakaranai;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == 'o') aru.insert(i);
+        if (s[i] == '?') wakaranai.insert(i);
+    }
+    int ans = 0;
+    for (int i = 0; i < 10000; i++) {
+        int pass = i;
+        set<int> num;
+        bool flag = true;
+        for (int j = 0; j < 4; j++) {
+            if (aru.count(pass % 10) != 0) num.insert(pass % 10);
+            if (aru.count(pass % 10) == 0 && wakaranai.count(pass % 10) == 0) {
+                flag = false;
+                break;
+            }
+            pass /= 10;
+        }
+        if (flag && num == aru) ans++;
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
