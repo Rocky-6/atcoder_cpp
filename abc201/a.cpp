a04560201ac826fa8c06e21aa44f000f11580c39
Rocky-6 <imu387058@gmail.com>
2023-07-25 15:50:29 +0900

    a

--- abc201/a.cpp
+++ abc201/a.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main() {
+    vector<int> a;
+    for (int i = 0; i < 3; i++) {
+        int tmp;
+        cin >> tmp;
+        a.push_back(tmp);
+    }
+    sort(a.begin(), a.end());
+    if (a[1] - a[0] == a[2] - a[1])
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 
     return 0;
 }
