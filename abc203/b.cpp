a1196d8e9bdb4a557230e5cbb88c5c9668253601
Rocky-6 <imu387058@gmail.com>
2023-07-26 00:27:27 +0900

    b

--- abc203/b.cpp
+++ abc203/b.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            ans += 100 * i + j;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
