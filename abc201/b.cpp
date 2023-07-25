50d831636d3a6f63e63771b47e25039d0702fa2d
Rocky-6 <imu387058@gmail.com>
2023-07-25 15:50:33 +0900

    b

--- abc201/b.cpp
+++ abc201/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<pair<string, int>> st;
+    for (int i = 0; i < n; i++) {
+        string s;
+        int t;
+        cin >> s >> t;
+        st.push_back(make_pair(s, t));
+    }
 
+    sort(st.begin(), st.end(), [](const auto& lhs, const auto& rhs) {
+        return lhs.second > rhs.second;
+    });
+
+    cout << st[1].first << endl;
     return 0;
 }
