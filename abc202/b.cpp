f6a63d5c0e72c40cb7feac6c62e837d5588d181d
Rocky-6 <imu387058@gmail.com>
2023-07-26 00:27:14 +0900

    b

--- abc202/b.cpp
+++ abc202/b.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+    for (auto &si : s) {
+        if (si == '6')
+            si = '9';
+        else if (si == '9')
+            si = '6';
+    }
+    for (int i = 0; i < s.size(); i++) {
+        cout << s[s.size() - 1 - i];
+    }
+    cout << endl;
 
     return 0;
 }
