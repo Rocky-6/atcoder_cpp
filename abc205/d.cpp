f442912b7d4b37a6878e3f98241129ba21c0259e
Rocky-6 <imu387058@gmail.com>
2023-07-26 18:14:06 +0900

    d

--- abc205/d.cpp
+++ abc205/d.cpp
@@ -1,8 +1,29 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<ll> a(n);
+    vector<ll> pa(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        pa[i] = a[i] - 1 - i;
+    }
+
+    while (q--) {
+        ll k;
+        cin >> k;
+        auto itr = lower_bound(pa.begin(), pa.end(), k);
+        int pos = itr - pa.begin();
+        if (itr == pa.end()) {
+            pos--;
+            cout << a[pos] + k - pa[pos] << endl;
+        } else
+            cout << k + pos << endl;
+    }
 
     return 0;
 }
