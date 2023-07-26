01be30e1a4068cd9e539edafe0f5737f01a0ede8
Rocky-6 <imu387058@gmail.com>
2023-07-26 18:13:47 +0900

    c

--- abc204/c.cpp
+++ abc204/c.cpp
@@ -3,6 +3,37 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> G(n);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        vector<bool> seen(n, false);
+        queue<int> que;
+        seen[i] = true;
+        ans++;
+        que.push(i);
+        while (!que.empty()) {
+            int v = que.front();
+            que.pop();
+            for (int nv : G[v]) {
+                if (seen[nv]) continue;
+                seen[nv] = true;
+                ans++;
+                que.push(nv);
+            }
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
