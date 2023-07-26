7a40574c09988e6edd79a2c8b55babe2321c9539
Rocky-6 <imu387058@gmail.com>
2023-07-26 18:13:39 +0900

    a

--- abc204/a.cpp
+++ abc204/a.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-
+    int x, y;
+    cin >> x >> y;
+    if (x == y)
+        cout << x << endl;
+    else
+        cout << 3 - x - y << endl;
     return 0;
 }
