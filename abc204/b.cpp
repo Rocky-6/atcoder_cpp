de0ceb42bc3bf618d3154a7175bc97c9333b80f1
Rocky-6 <imu387058@gmail.com>
2023-07-26 18:13:43 +0900

    b

--- abc204/b.cpp
+++ abc204/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] > 10)
+            a[i] -= 10;
+        else
+            a[i] = 0;
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += a[i];
+    }
+    cout << ans << endl;
     return 0;
 }
