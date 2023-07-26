bc5fe5d3bbf402df460b20cb0bb102f8170bd385
Rocky-6 <imu387058@gmail.com>
2023-07-26 18:13:58 +0900

    b

--- abc205/b.cpp
+++ abc205/b.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    set<int> st;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        st.insert(a);
+    }
+    if (st.size() == n)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 
     return 0;
 }
