059c5d07d0e52612b9939ca6af916b5e2a924025
Rocky-6 <imu387058@gmail.com>
2023-07-26 18:13:51 +0900

    d

--- abc204/d.cpp
+++ abc204/d.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> t(n);
+
+    bitset<100005> dp;
+    dp[0] = 1;
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> t[i];
+        total += t[i];
+        dp |= dp << t[i];
+    }
+    int ans = total;
+    for (int i = 0; i <= total; i++) {
+        if (dp[i]) ans = min(ans, max(i, total - i));
+    }
+    cout << ans << endl;
 
     return 0;
 }
