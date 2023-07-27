33e1bfbb7e58c8fe020f0e829457962382d5bd05
Rocky-6 <imu387058@gmail.com>
2023-07-27 18:08:40 +0900

    a

--- abc208/a.cpp
+++ abc208/a.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
+    int a, b;
+    cin >> a >> b;
+    if (b <= (6 * a) && b >= a)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 
     return 0;
 }
