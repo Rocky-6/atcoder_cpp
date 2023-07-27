3210b225cdbac6979b449d63259da25ff8e2196c
Rocky-6 <imu387058@gmail.com>
2023-07-27 05:40:33 +0000

    b

--- abc207/b.cpp
+++ abc207/b.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int diff = a + b - c * d;
+    if (diff >= a) {
+        cout << -1 << endl;
+        return 0;
+    }
 
+    int i = 0;
+    while (a > 0) {
+        a += b - c * d;
+        i++;
+    }
+    cout << i << endl;
     return 0;
 }
