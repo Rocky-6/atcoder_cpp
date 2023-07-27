0e8f2cb831938a93639cb14f58d173df5c14262f
Rocky-6 <imu387058@gmail.com>
2023-07-27 13:51:18 +0000

    a

--- abc309/a.cpp
+++ abc309/a.cpp
@@ -5,15 +5,9 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    if ((a - 1) / 3 != (b - 1) / 3) {
-        cout << "No" << endl;
-        return 0;
-    }
-    if (abs(a - b) != 1) {
-        cout << "No" << endl;
-        return 0;
-    }
-    cout << "Yes" << endl;
-
+    if (b - a < 0)
+        cout << 0 << endl;
+    else
+        cout << b - a + 1 << endl;
     return 0;
 }
