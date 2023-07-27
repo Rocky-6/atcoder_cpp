3b2aeea2a93378bf80c8a1494d45b14171b1d54e
Rocky-6 <imu387058@gmail.com>
2023-07-27 13:56:31 +0000

    b

--- abc209/b.cpp
+++ abc209/b.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+    int n, x;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (i % 2 == 1) a--;
+        x -= a;
+    }
+    if (x >= 0)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 
     return 0;
 }
