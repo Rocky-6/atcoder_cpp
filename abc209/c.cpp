958c6fa506cc3992699762386a9f72ca15e5c655
Rocky-6 <imu387058@gmail.com>
2023-07-27 13:56:34 +0000

    c

--- abc209/c.cpp
+++ abc209/c.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
+const ll mod = 1e9 + 7;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+    sort(c.begin(), c.end());
+    int minus = 0;
+    ll ans = 1;
+    for (int i = 0; i < n; i++) {
+        ans *= c[i] - minus;
+        ans %= mod;
+        minus++;
+    }
+    cout << ans << endl;
 
     return 0;
 }
