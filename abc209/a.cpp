92bcd69f700525e1d8941efcd472556ad7e3f77d
Rocky-6 <imu387058@gmail.com>
2023-07-27 13:56:27 +0000

    a

--- abc209/a.cpp
+++ abc209/a.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-
+    int a, b;
+    cin >> a >> b;
+    if (b - a < 0)
+        cout << 0 << endl;
+    else
+        cout << b - a + 1 << endl;
     return 0;
 }
