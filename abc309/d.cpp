eda9e7503493cb54d9f73c07a1f61b57340dfafd
Rocky-6 <imu387058@gmail.com>
2023-07-27 13:51:36 +0000

    d

--- abc309/d.cpp
+++ abc309/d.cpp
@@ -3,76 +3,44 @@
 using namespace std;
 
 int main() {
-    int n1, n2, m;
-    cin >> n1 >> n2 >> m;
-    vector<vector<int>> G1(n1), G2(n2);
-    for (int i = 0; i < m; ++i) {
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> G(n);
+    for (int i = 0; i < n - 1; i++) {
         int a, b;
         cin >> a >> b;
-        if (b <= n1) {
-            a--;
-            b--;
-            G1[a].push_back(b);
-            G1[b].push_back(a);
-        }
-        if (a >= n1 + 1) {
-            a -= n1;
-            b -= n1;
-            a--;
-            b--;
-            G2[a].push_back(b);
-            G2[b].push_back(a);
-        }
+        a--;
+        b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
     }
 
-    vector<int> dist(n1, -1);
     queue<int> que;
-
-    dist[0] = 0;
+    vector<bool> seen(n), odd(n);
     que.push(0);
-    int maxdist = 0;
-    int maxpoint = 0;
+    seen[0] = true;
+    odd[0] = true;
     while (!que.empty()) {
         int v = que.front();
         que.pop();
-
-        for (int nv : G1[v]) {
-            if (dist[nv] != -1) continue;
-            dist[nv] = dist[v] + 1;
-            if (maxdist < dist[nv]) {
-                maxpoint = nv;
-                maxdist = dist[nv];
-            }
+        for (auto nv : G[v]) {
+            if (seen[nv]) continue;
+            seen[nv] = true;
+            odd[nv] = !odd[v];
             que.push(nv);
         }
     }
 
-    int maxdist1 = maxdist;
-
-    dist.assign(n2, -1);
-
-    dist[n2 - 1] = 0;
-    que.push(n2 - 1);
-    maxdist = 0;
-    maxpoint = 0;
-    while (!que.empty()) {
-        int v = que.front();
-        que.pop();
-
-        for (int nv : G2[v]) {
-            if (dist[nv] != -1) continue;
-            dist[nv] = dist[v] + 1;
-            if (maxdist < dist[nv]) {
-                maxpoint = nv;
-                maxdist = dist[nv];
-            }
-            que.push(nv);
-        }
+    for (int i = 0; i < q; i++) {
+        int c, d;
+        cin >> c >> d;
+        c--;
+        d--;
+        if (odd[c] == odd[d])
+            cout << "Town" << endl;
+        else
+            cout << "Road" << endl;
     }
-
-    int maxdist2 = maxdist;
-
-    cout << maxdist1 + maxdist2 + 1 << endl;
 
     return 0;
 }
