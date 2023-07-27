bca4125ef295011fa51bf1d3fa412a72f4cd6417
Rocky-6 <imu387058@gmail.com>
2023-07-27 05:40:39 +0000

    c

--- abc207/c.cpp
+++ abc207/c.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
-int main() {
+bool f(pair<double, double> i, pair<double, double> j) {
+    if (i.first >= j.first && i.first <= j.second) return true;
+    if (i.second >= j.first && i.second <= j.second) return true;
+    if (j.first >= i.first && j.first <= i.second) return true;
+    if (j.second >= i.first && j.second <= i.second) return true;
+    return false;
+}
 
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<double, double>> lr;
+    for (int i = 0; i < n; i++) {
+        int t;
+        double l, r;
+        cin >> t >> l >> r;
+        if (t == 2) r -= 0.1;
+        if (t == 3) l += 0.1;
+        if (t == 4) l += 0.1, r -= 0.1;
+        lr.push_back(make_pair(l, r));
+    }
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (f(lr[i], lr[j])) ans++;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
