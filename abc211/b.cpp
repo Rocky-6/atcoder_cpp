b9176aecd5d124c6fd46e46462a99dd5584ce1c0
Rocky-6 <imu387058@gmail.com>
2023-07-28 15:49:20 +0900

    b

--- abc211/b.cpp
+++ abc211/b.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
-
+    set<string> st;
+    for (int i = 0; i < 4; i++) {
+        string s;
+        cin >> s;
+        st.insert(s);
+    }
+    if (st.size() == 4)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
     return 0;
 }
