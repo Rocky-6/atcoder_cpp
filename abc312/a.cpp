7f42f6a6832652a911e02a68c01c6ea94ef6e8f4
Rocky-6 <imu387058@gmail.com>
2023-08-05 11:54:15 +0000

    a

--- abc312/a.cpp
+++ abc312/a.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
-
+    string s;
+    cin >> s;
+    auto chord = {"ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD"};
+    for (auto c : chord) {
+        if (c == s) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
