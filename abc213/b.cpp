7f396bbb1b4e61f42a33860fee60f6d833b6520e
Rocky-6 <imu387058@gmail.com>
2023-08-07 23:14:06 +0000

    b

--- abc213/b.cpp
+++ abc213/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        int ax;
+        cin >> ax;
+        a[i] = make_pair(i + 1, ax);
+    }
+
+    sort(a.begin(), a.end(), [](const auto& lhs, const auto& rhs) {
+        return lhs.second < rhs.second;
+    });
+
+    cout << a[n - 2].first << endl;
 
     return 0;
 }
