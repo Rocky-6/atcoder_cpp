8d44f4166d6c845c5d161d772b40916ddd26baf0
Rocky-6 <imu387058@gmail.com>
2023-08-07 19:12:26 +0900

    d

--- abc212/d.cpp
+++ abc212/d.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+using ll = long long;
 int main() {
-
+    int q;
+    cin >> q;
+    priority_queue<ll, vector<ll>, greater<ll>> que;
+    ll s = 0;
+    while (q--) {
+        int query;
+        cin >> query;
+        switch (query) {
+            case 1:
+                ll x1;
+                cin >> x1;
+                que.push(x1 - s);
+                break;
+            case 2:
+                ll x2;
+                cin >> x2;
+                s += x2;
+                break;
+            case 3:
+                cout << que.top() + s << endl;
+                que.pop();
+            default:
+                break;
+        }
+    }
     return 0;
 }
