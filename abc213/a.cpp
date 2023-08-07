56119ce347ecf340e5978943aa5237012f7a7d02
Rocky-6 <imu387058@gmail.com>
2023-08-07 23:14:02 +0000

    a

--- abc213/a.cpp
+++ abc213/a.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
+    int a, b;
+    cin >> a >> b;
+    for (int i = 0; i < 256; i++) {
+        if ((a ^ i) == b) {
+            cout << i << endl;
+            return 0;
+        }
+    }
 
     return 0;
 }
