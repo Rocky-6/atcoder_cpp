a4601c4cf4a06c4f381630201a74a8baa3f923d7
Rocky-6 <imu387058@gmail.com>
2023-08-07 18:58:43 +0900

    a

--- abc212/a.cpp
+++ abc212/a.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-
+    int a, b;
+    cin >> a >> b;
+    if (0 < a && b == 0)
+        cout << "Gold" << endl;
+    else if (a == 0 && 0 < b)
+        cout << "Silver" << endl;
+    else if (0 < a && 0 < b)
+        cout << "Alloy" << endl;
     return 0;
 }
