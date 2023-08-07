9301f06eac58d4804588d27afad4438dce69016a
Rocky-6 <imu387058@gmail.com>
2023-08-07 18:58:47 +0900

    b

--- abc212/b.cpp
+++ abc212/b.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 int main() {
+    string x;
+    cin >> x;
+    bool flag = true;
+    for (int i = 0; i < 3; i++) {
+        if (x[i + 1] != x[i]) flag = false;
+    }
+    if (flag) {
+        cout << "Weak" << endl;
+        return 0;
+    }
+
+    flag = true;
+    for (int i = 0; i < 3; i++) {
+        if (x[i + 1] - '0' != ((x[i] - '0') + 1) % 10) flag = false;
+    }
+    if (flag) {
+        cout << "Weak" << endl;
+        return 0;
+    }
+
+    cout << "Strong" << endl;
 
     return 0;
 }
