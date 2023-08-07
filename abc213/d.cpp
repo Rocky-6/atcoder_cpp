0dd10ecd009d94022950c17257ca40a03719fd60
Rocky-6 <imu387058@gmail.com>
2023-08-07 23:14:10 +0000

    d

--- abc213/d.cpp
+++ abc213/d.cpp
@@ -3,6 +3,33 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> G(n);
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    for (int i = 0; i < n; i++) {
+        sort(G[i].begin(), G[i].end());
+    }
 
+    vector<bool> seen(n, false);
+    auto dfs = [&](auto dfs, int v) -> void {
+        seen[v] = true;
+        cout << v + 1 << ' ';
+        for (auto nv : G[v]) {
+            if (seen[nv]) continue;
+            dfs(dfs, nv);
+            cout << v + 1 << ' ';
+        }
+    };
+
+    dfs(dfs, 0);
+    cout << endl;
     return 0;
 }
