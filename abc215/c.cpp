fb5b4d0b08212377a79b551c9ea7b948d40089c0
Rocky-6 <imu387058@gmail.com>
2023-08-08 18:36:42 +0900

    c

--- abc215/c.cpp
+++ abc215/c.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-
+    string s;
+    int k;
+    cin >> s >> k;
+    sort(s.begin(), s.end());
+    for (int i = 0; i < k - 1; i++) {
+        next_permutation(s.begin(), s.end());
+    }
+    cout << s << endl;
     return 0;
 }
