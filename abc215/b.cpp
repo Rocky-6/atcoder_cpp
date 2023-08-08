549e592258ae2987fbcb1e809bc5e6b411d44f1b
Rocky-6 <imu387058@gmail.com>
2023-08-08 18:36:39 +0900

    b

--- abc215/b.cpp
+++ abc215/b.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
-
+    ll n;
+    cin >> n;
+    int k = 0;
+    while (n >= 2) {
+        n /= 2;
+        k++;
+    }
+    cout << k << endl;
     return 0;
 }
