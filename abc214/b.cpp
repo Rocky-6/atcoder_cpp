7e9b0141efea8de3041157af5cc240627b147956
Rocky-6 <imu387058@gmail.com>
2023-08-08 14:40:44 +0900

    b

--- abc214/b.cpp
+++ abc214/b.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-
+    int s, t;
+    cin >> s >> t;
+    long long ans = 0;
+    for (int a = 0; a <= s; a++) {
+        for (int b = 0; b <= s - a; b++) {
+            for (int c = 0; c <= s - a - b; c++) {
+                if (a * b * c <= t) ans++;
+            }
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
