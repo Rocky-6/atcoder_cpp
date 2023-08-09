a80790f2b2328b75ece6ea02572459128bc0aafa
Rocky-6 <imu387058@gmail.com>
2023-08-09 00:44:04 +0000

    a

--- abc216/a.cpp
+++ abc216/a.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-
+    int x, y;
+    char sep;
+    cin >> x >> sep >> y;
+    cout << x;
+    if (y >= 0 && y <= 2) cout << '-';
+    if (y >= 7 && y <= 9) cout << '+';
+    cout << endl;
     return 0;
 }
