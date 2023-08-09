aab0ffc0c8610a072b887c464d722da5b506f046
Rocky-6 <imu387058@gmail.com>
2023-08-09 00:44:07 +0000

    b

--- abc216/b.cpp
+++ abc216/b.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    set<pair<string, string>> st;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string si, ti;
+        cin >> si >> ti;
+        if (st.count(make_pair(si, ti)) != 0) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+        st.insert(make_pair(si, ti));
+    }
+    cout << "No" << endl;
     return 0;
 }
