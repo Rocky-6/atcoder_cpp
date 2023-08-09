0125034af3c37b45c81d18cc6105195c721bc971
Rocky-6 <imu387058@gmail.com>
2023-08-09 00:44:10 +0000

    c

--- abc216/c.cpp
+++ abc216/c.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
-
+    ll n;
+    cin >> n;
+    vector<char> ans;
+    while (n > 0) {
+        if (n % 2 == 0) {
+            n /= 2;
+            ans.push_back('B');
+        } else {
+            n--;
+            ans.push_back('A');
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    for (auto a : ans) cout << a;
+    cout << endl;
     return 0;
 }
