c6192d01413be97a605858267e2ba399a930dae8
Rocky-6 <imu387058@gmail.com>
2023-08-13 20:58:31 +0000

    b

--- abc218/b.cpp
+++ abc218/b.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() { return 0; }
+int main() {
+    for (int i = 0; i < 26; i++) {
+        int p;
+        cin >> p;
+        cout << (char)('a' + (p - 1));
+    }
+    cout << endl;
+    return 0;
+}
