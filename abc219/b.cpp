478d9f7d9d28c1241b552f4dc56aaabf524d1513
Rocky-6 <imu387058@gmail.com>
2023-08-15 14:24:27 +0000

    b

--- abc219/b.cpp
+++ abc219/b.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-
+    vector<string> s(3);
+    cin >> s[0] >> s[1] >> s[2];
+    string t;
+    cin >> t;
+    for (char tx : t) cout << s[tx - '1'];
+    cout << endl;
     return 0;
 }
