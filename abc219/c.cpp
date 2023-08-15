c10cc8f25b4b912b5623116b92ebd76ac306c44c
Rocky-6 <imu387058@gmail.com>
2023-08-15 14:24:34 +0000

    c

--- abc219/c.cpp
+++ abc219/c.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main() {
+    string x;
+    cin >> x;
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    std::sort(
+        s.begin(), s.end(), [&x](const std::string &a, const std::string &b) {
+            // 文字列aとbを新しいアルファベットの順序に基づいて比較
+            for (size_t i = 0; i < std::min(a.length(), b.length()); ++i) {
+                size_t indexA = x.find(a[i]);
+                size_t indexB = x.find(b[i]);
+                if (indexA != indexB) {
+                    return indexA < indexB;
+                }
+            }
+            return a.length() < b.length();
+        });
+
+    for (auto si : s) cout << si << endl;
 
     return 0;
 }
