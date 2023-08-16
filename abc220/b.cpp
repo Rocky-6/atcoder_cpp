c86d00d59ee07a39348ada0cb4555563e1d4e781
Rocky-6 <imu387058@gmail.com>
2023-08-16 15:04:34 +0000

    b

--- abc220/b.cpp
+++ abc220/b.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
-
+    int k;
+    cin >> k;
+    string a, b;
+    cin >> a >> b;
+    int ax = 0, bx = 0;
+    for (auto c : a) {
+        int cx = c - '0';
+        ax = ax * k + cx;
+    }
+    for (auto c : b) {
+        int cx = c - '0';
+        bx = bx * k + cx;
+    }
+    cout << (long long)ax * bx << endl;
     return 0;
 }
