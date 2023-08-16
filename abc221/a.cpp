7a534ee8851523894375a92f458a27053a992d1d
Rocky-6 <imu387058@gmail.com>
2023-08-16 15:04:41 +0000

    a

--- abc221/a.cpp
+++ abc221/a.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-
+    int a, b;
+    cin >> a >> b;
+    long long ans = 1;
+    while (a > b) {
+        ans *= 32;
+        a--;
+    }
+    cout << ans << endl;
     return 0;
 }
