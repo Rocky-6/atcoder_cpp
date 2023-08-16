20460e4c324a86baec14148487e7069111dd194d
Rocky-6 <imu387058@gmail.com>
2023-08-16 15:04:45 +0000

    b

--- abc221/b.cpp
+++ abc221/b.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main() {
+    string s, t;
+    cin >> s >> t;
+    int n = s.size();
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] != t[i]) {
+            if (cnt == 1) {
+                cout << "No" << endl;
+                return 0;
+            }
+            cnt++;
+            if (s[i] == t[i + 1] && s[i + 1] == t[i])
+                i++;
+            else {
+                cout << "No" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "Yes" << endl;
 
     return 0;
 }
