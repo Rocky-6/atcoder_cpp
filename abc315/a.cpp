35243b0f1a93b451144d6b317e38077bdce8c784
Rocky-6 <imu387058@gmail.com>
2023-08-19 23:03:45 +0900

    a

--- abc315/a.cpp
+++ abc315/a.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
-
+    string s;
+    cin >> s;
+    string aiueo = "aiueo";
+    string ans;
+    for (int i = 0; i < s.size(); i++) {
+        bool flag = true;
+        for (int j = 0; j < 5; j++) {
+            if (s[i] == aiueo[j]) {
+                flag = false;
+            }
+        }
+        if (flag) ans += s[i];
+    }
+    cout << ans << endl;
     return 0;
 }
