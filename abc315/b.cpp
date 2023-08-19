fe8f2862b387d87baabfa18c7c6cd9ce776d0f8c
Rocky-6 <imu387058@gmail.com>
2023-08-19 23:03:50 +0900

    b

--- abc315/b.cpp
+++ abc315/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    int m;
+    cin >> m;
+    vector<int> sum(m + 1);
+    for (int i = 0; i < m; i++) {
+        int d;
+        cin >> d;
+        sum[i + 1] = sum[i] + d;
+    }
+
+    int a = lower_bound(sum.begin(), sum.end(), (sum[m] + 1) / 2) - sum.begin();
+
+    int b = (sum[m] + 1) / 2 - sum[a - 1];
+
+    cout << a << ' ' << b << endl;
 
     return 0;
 }
